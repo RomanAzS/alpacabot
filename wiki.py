cbf49be8a2a16ed02ab1da0bf3a0ee401ea2d585
A-ro <azi.r3d@gmail.com>
2013-08-09 09:32:17 +0000

    Fixed the regex in wiki.py

--- wiki.py
+++ wiki.py
@@ -9,21 +9,32 @@ def wiki(msg):
     response = response.read()
     response = response.split("<p>")
     res = response[1].split("p>")
-    if "may refer to" in res[0]:
+    if "may refer to" in res[0] or "can refer to" in res[0]:
         res = res[0]
         res = re.sub("<.*?>", '', res)
         return "%s http://en.wikipedia.org/wiki/%s" % (res.strip("/<"), msg)
     else:
         while "<tr>" in response[1]:
             del response[1]
+        while "td>" in response[1]:
+            del response[1]
+        while "<li>" in response[1]:
+            del response[1]
         response = response[1]
         response = re.sub("<.*?>", '', response)
-        response = re.sub("\[\d?\]", '', response)
-#        response = response.split('.')
-        response = re.split("[A-Z][^\.!?] *?[\.!?]", response)
-        print(str(response))
-        if (len(response[0]) + len(response[1])) >= 450:
-            resp = response[0]
-        else:
-            resp = "%s.%s." %(response[0], response[1])
+        response = re.sub("\[\d?\]", '', response) 
+#        response = response.split('.') 
+#        response = re.split("[A-Z][A-Za-z0-9\"\[\]\'\(\);,:&\s]+ *?[\.!?]", response) # broke it
+        d = re.findall(".+?(?<!Dr|Mr|Jr|Sr| \w|\.\w)[.!?]\s", response)
+#        response = re.split("[.!?] ", response)
+        print d
+        try:
+            length = len(d[0]) + len(d[1])
+            if (length) >= 450:
+                resp = d[0].strip()
+            else:
+                
+                resp = "%s %s" %(d[0].strip(), d[1].strip())
+        except:
+            resp = d[0].strip()
         return resp.strip()
